bcaa6eb40d94b466c47f19be2e52beb587a5255d
kit365 <kietops365@gmail.com>
2025-03-24 21:12:39 +0700

    add Thymeleaf configuration and create Home page template
    Signed-off-by: kit365 <kietops@gmail.com>

---
+++ src/main/java/com/fpt/enewsvn/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.fpt.enewsvn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Home";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        return "Home";
+    }
+}
