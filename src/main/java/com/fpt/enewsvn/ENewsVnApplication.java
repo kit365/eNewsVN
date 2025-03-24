958493bc856b6395f3fd59e962d683ba12a3a08b
kit365 <kietops365@gmail.com>
2025-03-24 20:31:15 +0700

    initialize project structure with Spring Boot and Maven setup
    Signed-off-by: kit365 <kietops@gmail.com>

---
+++ src/main/java/com/fpt/enewsvn/ENewsVnApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.enewsvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ENewsVnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ENewsVnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fpt/enewsvn/ENewsVnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.enewsvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ENewsVnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
