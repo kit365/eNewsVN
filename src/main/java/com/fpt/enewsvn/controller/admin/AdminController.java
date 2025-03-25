45a44016352eb68644a6567350efafd9d7d23b3c
kit365 <kietops365@gmail.com>
2025-03-25 13:51:44 +0700

    add functionality for editing blog categories; implement modal for category updates and enhance category display
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/fpt/enewsvn/controller/admin/AdminController.java
+++ src/main/java/com/fpt/enewsvn/controller/admin/AdminController.java
@@ -1,10 +1,13 @@
 package com.fpt.enewsvn.controller.admin;
 
 import com.fpt.enewsvn.dto.request.blog_category.CreateBlogCategoryRequest;
+import com.fpt.enewsvn.dto.request.blog_category.UpdateBlogCategoryRequest;
 import com.fpt.enewsvn.dto.response.BlogCategoryResponse;
+import com.fpt.enewsvn.entity.BlogCategoryEntity;
 import com.fpt.enewsvn.service.BlogCategoryService;
 import com.fpt.enewsvn.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,28 +29,44 @@ public class AdminController {
     }
 
     @GetMapping("/blog-category")
-    public String blogcategory(Model model) {
+    public String blogCategory(Model model) {
         List<BlogCategoryResponse> list = blogCategoryService.getAll();
         model.addAttribute("blogs", list);
         return "categories";
     }
 
-    @DeleteMapping("blogs/delete/{id}")
+
+
+
+    @DeleteMapping("/blogs/delete/{id}")
     public String deleteBlog(@PathVariable("id") Long id) {
         blogService.delete(id);
-        return "redirect:/blogs";
+        return "redirect:/admin/blogs"; // Đảm bảo redirect đến đúng URL
     }
 
     @PostMapping("/category/add")
     public String addCategory(@ModelAttribute CreateBlogCategoryRequest request) {
-     blogCategoryService.add(request);
+        blogCategoryService.add(request);
         return "redirect:/admin/blog-category";
     }
 
     @GetMapping("/category/delete/{id}")
-    public String addCategory(@PathVariable("id") Long id) {
+    public String deleteCategory(@PathVariable("id") Long id) {
         blogCategoryService.delete(id);
         return "redirect:/admin/blog-category";
     }
 
+    @GetMapping("/category/{id}")
+    @ResponseBody // Sử dụng để trả về JSON
+    public BlogCategoryResponse showCategoryByID(@PathVariable("id") Long id) {
+        return blogCategoryService.showDetail(id);
+    }
+
+    @PostMapping("/category/edit/{id}")
+    public ResponseEntity<String> editCategory(@PathVariable("id") Long id, @RequestBody UpdateBlogCategoryRequest request) {
+        System.out.println("test");
+        blogCategoryService.update(id, request);
+        System.out.println(request);
+        return ResponseEntity.ok("Cập nhật thành công"); // Hoặc trả về thông báo khác
+    }
 }
