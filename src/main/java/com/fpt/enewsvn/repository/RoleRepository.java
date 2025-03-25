d90ed447c5ff977748038196200c31a97f9e6abd
Doan Minh Phu <phujason1992@gmail.com>
2025-03-25 20:18:22 +0700

    add role management functionality; implement RoleService, update AdminController, and enhance role views

--- src/main/java/com/fpt/enewsvn/repository/RoleRepository.java
+++ src/main/java/com/fpt/enewsvn/repository/RoleRepository.java
@@ -1,6 +1,9 @@
 package com.fpt.enewsvn.repository;
 
+import com.fpt.enewsvn.Enum.Status;
 import com.fpt.enewsvn.entity.RoleEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,5 +16,6 @@ public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
 
     Optional<RoleEntity> findByTitle(String title);
 
+    Page<RoleEntity> findByTitleContainingAndStatus(String title, Status status, Pageable pageable);
 
 }
